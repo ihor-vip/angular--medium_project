48bff66d08d1df57675527308c39085469a785bf
ihor-vip <vip20140902@gmail.com>
2023-03-14 13:02:13 +0200

    create selector for auth module

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,7 +1,10 @@
 import {Component, OnInit} from '@angular/core'
 import {FormBuilder, FormGroup, Validators} from '@angular/forms'
-import {Store} from '@ngrx/store'
+import {select, Store} from '@ngrx/store'
 import {registerAction} from '../../store/actions/register.action'
+import {Observable} from 'rxjs'
+import {isSubmittingSelector} from '../../store/selectors'
+import {AppStateInterface} from '../../../shared/types/appState.interface'
 
 @Component({
   selector: 'mc-register',
@@ -10,14 +13,23 @@ import {registerAction} from '../../store/actions/register.action'
 })
 export class RegisterComponent implements OnInit {
   form: FormGroup
+  isSubmitting$: Observable<boolean>
 
-  constructor(private fb: FormBuilder, private store: Store) {}
+  constructor(
+    private fb: FormBuilder,
+    private store: Store<AppStateInterface>
+  ) {}
 
   ngOnInit(): void {
     this.initializeForm()
+    this.initializeValues()
   }
 
-  initializeForm() {
+  private initializeValues(): void {
+    this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector))
+  }
+
+  initializeForm(): void {
     this.form = this.fb.group({
       username: ['', Validators.required],
       email: '',
