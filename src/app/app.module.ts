ef53dabbd4814b2e4fbc7944cce2b3f28295bb5f
ihor-vip <vip20140902@gmail.com>
2023-03-15 16:50:29 +0200

    create interceptor for authorization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,11 @@ import {AuthModule} from './auth/auth.module'
 import {StoreModule} from '@ngrx/store'
 import {StoreDevtoolsModule} from '@ngrx/store-devtools'
 import {environment} from '../environments/environment'
-import {HttpClientModule} from '@angular/common/http'
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http'
 import {EffectsModule} from '@ngrx/effects'
 import {TopBarModule} from './shared/modules/topBar/topBar.module'
+import {PersistanceService} from './shared/services/persistance.service'
+import {AuthInterceptor} from './shared/services/authinterceptor.service'
 
 @NgModule({
   declarations: [AppComponent],
@@ -26,7 +28,14 @@ import {TopBarModule} from './shared/modules/topBar/topBar.module'
     }),
     TopBarModule,
   ],
-  providers: [],
+  providers: [
+    PersistanceService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
