ac8bdfbc39970a52d27ec2139272c050447e12d5
ihor-vip <vip20140902@gmail.com>
2023-03-19 09:37:26 +0200

    create store add to favorite

--- src/app/shared/modules/addToFavorites/addToFavorites.module.ts
+++ src/app/shared/modules/addToFavorites/addToFavorites.module.ts
@@ -1,12 +1,14 @@
 import {NgModule} from '@angular/core'
 import {CommonModule} from '@angular/common'
-import {EffectsModule} from '@ngrx/effects'
 import {AddToFavoritesComponent} from './components/addToFavorites/addToFavorites.component'
+import {AddToFavoritesService} from './services/addToFavorites.service'
+import {EffectsModule} from '@ngrx/effects'
+import {AddToFavoriteEffect} from './store/effects/addToFavorite.effect'
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, EffectsModule.forFeature([AddToFavoriteEffect])],
   declarations: [AddToFavoritesComponent],
   exports: [AddToFavoritesComponent],
-  providers: [],
+  providers: [AddToFavoritesService],
 })
 export class AddToFavoritesModule {}
--- src/app/shared/modules/addToFavorites/components/addToFavorites/addToFavorites.component.ts
+++ src/app/shared/modules/addToFavorites/components/addToFavorites/addToFavorites.component.ts
@@ -1,4 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core'
+import {Store} from '@ngrx/store'
+import {addToFavoritesAction} from '../../store/actions/addToFavorites.action'
 
 @Component({
   selector: 'mc-add-to-favorites',
@@ -13,12 +15,20 @@ export class AddToFavoritesComponent implements OnInit {
   favoritesCount: number
   isFavorited: boolean
 
+  constructor(private store: Store) {}
+
   ngOnInit(): void {
     this.favoritesCount = this.favoritesCountProps
     this.isFavorited = this.isFavoritedProps
   }
 
   handleLike(): void {
+    this.store.dispatch(
+      addToFavoritesAction({
+        isFavorited: this.isFavorited,
+        slug: this.articleSlugProps,
+      })
+    )
     if (this.isFavorited) {
       this.favoritesCount = this.favoritesCount - 1
     } else {
